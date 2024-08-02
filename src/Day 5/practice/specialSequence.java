aad5150ea43ca4d29cdd20131dc389be8146a4c1
haarish04 <haarish.anandan@gmail.com>
2024-08-02 10:32:32 +0530

    Updated sequence question

--- src/Day 5/practice/specialSequence.java
+++ src/Day 5/practice/specialSequence.java
@@ -6,21 +6,20 @@ public class specialSequence {
         int num3= 3;
 
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number: ");
         int n= sc.nextInt();
 
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(num3);
-        for(int i=2;i<n;i++)
-            System.out.println(sequence(i));
+        System.out.print(num1 + " " + num2 + " " + num3);
+        int nextNum = num1 + num2 + num3;
+        while (nextNum <= n) {
+            System.out.print(" " + nextNum);
+            num1 = num2;
+            num2 = num3;
+            num3 = nextNum;
+            nextNum = num1 + num2 + num3;
+        }
         sc.close();
     }
 
-    public static int sequence(int i){
-        if(i<=3)
-            return i;
-        return sequence(i-1) + sequence(i-2)+ sequence(i-3);
-        
-    }
     
 }
