c8f2125dd549492f3a530319dc1429b5d48798da
haarish04 <haarish.anandan@gmail.com>
2024-08-02 10:11:26 +0530

    Updated CLI programs

---
+++ src/Day 5/practice/commandTest.java
@@ -0,0 +1,10 @@
+public class commandTest {
+    public static void main(String[] args) {
+        String args1= args[0];
+        String args2= args[1];
+
+        System.out.println(args1);
+        System.out.println(args2);
+    }
+    
+}
