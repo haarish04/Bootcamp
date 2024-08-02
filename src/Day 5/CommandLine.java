201853a7db96f0c7deb6ad3222ed3b9445c6a21c
haarish04 <haarish.anandan@gmail.com>
2024-08-02 09:46:03 +0530

    Updated day 5

---
+++ src/Day 5/CommandLine.java
@@ -0,0 +1,8 @@
+public class CommandLine {
+    public static void main(String[] args){
+        System.out.println("Enter input: ");
+        int i=Integer.parseInt(args[0]);
+        System.out.println(i);
+    }
+    
+}
