863c4e2763c41fc38878941c17851a494c598b25
haarish04 <haarish.anandan@gmail.com>
2024-08-05 10:39:15 +0530

    Updated dumb error

--- src/Day 4/FirstOccurence.java
+++ src/Day 4/FirstOccurence.java
@@ -12,9 +12,10 @@ public class FirstOccurence {
 
         int pos=-1;
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==n)
+            if(arr[i]==n){
                 pos=i+1;
                 break;
+            }
         }
 
         System.out.println(pos);
