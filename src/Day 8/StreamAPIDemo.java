eaa91ecc6cec41e2e6943793aa2ec2bad7fde7c7
haarish04 <haarish.anandan@gmail.com>
2024-08-07 13:45:59 +0530

    Trying out functions with streams StreamAPI

--- src/Day 8/StreamAPIDemo.java
+++ src/Day 8/StreamAPIDemo.java
@@ -6,10 +6,20 @@ public class StreamAPIDemo {
         
         System.out.println(numbers.stream().count());
 
+        //forEach performs some action (consumer) specified by our lambda function and applies it to every element in th estream
+        //Consumer is a functional interface
         numbers.stream().forEach(num -> {
             System.out.println(num);
         });
 
+        //Use optional to accept null value as well
+        Optional<Integer> res =numbers.stream().reduce((num1, num2) ->(num1 +num2));
+
+        //Use orElse to accept null and modify it to some other value of our choice
+        int res2 =numbers.stream().reduce((num1, num2) ->(num1 +num2)).orElse(0);
+
+        System.out.println(res.get());
+        System.out.println(res2);
     }
     
 }
